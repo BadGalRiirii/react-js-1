81a3e33e075f334171fcdc399cf97e17b29c5c91
BadGalRiirii <nicolasruilan30@gmail.com>
2024-05-08 21:20:35 +0800

    initial commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Message(props) {
   return (
     <div className="bg-green-100 p-4">
-      <h1 className="text-green-900 text-xl font-bold">This is a functional component</h1>
+      <h1 className="text-green-800 text-xl font-bold">This is a functional component</h1>
     </div>
   );
 }
